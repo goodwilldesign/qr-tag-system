049d9deac504020854d04e87a0369b2800bd6643
ajayjob2011 <89439966+ajayjob2011@users.noreply.github.com>
2026-03-24 22:00:47 +0530

    feat: set PWA start_url to /login

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
+        start_url: '/login',
         icons: [
           {
             src: 'pwa-64x64.png',
